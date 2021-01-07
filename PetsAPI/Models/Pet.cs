868bed35310d594b23938e62db9e76f463247ec4
gulangurman <gulangurman@gmail.com>
2021-01-07 23:04:05 +0300

    First commit.

---
+++ PetsAPI/Models/Pet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PetsAPI.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Details { get; set; }
+        public int BirthYear { get; set; }
+    }
+}
---
+++ PetsAPI/Models/PetDTO.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace PetsAPI.Models
+{
+    public class PetDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
